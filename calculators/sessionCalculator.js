765ef75d19115c9a745cb7b0f8b5d1899f324ae0
Michael Klein <miketheklein@gmail.com>
2017-04-11 16:39:14 -0700

    WIP first test passing

--- calculators/sessionCalculator.js
+++ calculators/sessionCalculator.js
@@ -16,28 +16,27 @@ module.exports = function(session1StartDate){
   const session4StartDate = toNextMonday(session3EndDate)
   const session4EndDate   = calculateSessionEndDate(session4StartDate)
 
-  console.log('-=-=-=-=- Session1')
   const numberOfDaysInSession1 = calculateNumberOfDaysInSession(
     session1StartDate,
     session1EndDate
   )
-  console.log('-=-=-=-=- Session2')
   const numberOfDaysInSession2 = calculateNumberOfDaysInSession(
     session2StartDate,
     session2EndDate
   )
-  console.log('-=-=-=-=- Session3')
   const numberOfDaysInSession3 = calculateNumberOfDaysInSession(
     session3StartDate,
     session3EndDate
   )
-  console.log('-=-=-=-=- Session4')
   const numberOfDaysInSession4 = calculateNumberOfDaysInSession(
     session4StartDate,
     session4EndDate
   )
+
+  const cancellationDate = toNextMonday(session1StartDate.clone().add(35, 'days'))
+
   return {
-    // cancellationDate,
+    cancellationDate:  dateToString(cancellationDate),
     session1StartDate: dateToString(session1StartDate),
     session1EndDate:   dateToString(session1EndDate),
     session2StartDate: dateToString(session2StartDate),
@@ -60,10 +59,8 @@ const dateToString = function(date){
 }
 
 const toNextMonday = function(date){
-  return moment(date)
-    .clone()
-    .endOf('week')
-    .add(2, 'days')
+  if (moment(date).format('dddd') === 'Monday') return date
+  return moment(date).endOf('week').add(2, 'days')
 }
 
 const calculateSessionEndDate = function(sessionStartDate){
@@ -86,9 +83,6 @@ const calculateSessionEndDate = function(sessionStartDate){
 }
 
 const calculateNumberOfDaysInSession = function(from, to){
-  console.log('from', from.format('dddd YYYY-MM-DD'))
-  console.log('to  ', to.format('dddd YYYY-MM-DD'))
-
   let numberOfDays = moment(to).diff(from, 'days')
 
   const allDays = generateObjectOfDates(from, numberOfDays)
@@ -104,26 +98,11 @@ const calculateNumberOfDaysInSession = function(from, to){
     const thursday  = dateToString(moment(weekBreak).add(3, 'days'))
     const friday    = dateToString(moment(weekBreak).add(4, 'days'))
 
-    if (monday    in allDays) {
-      console.log('accounting for', monday, 'within week break', weekBreak)
-      allDays[monday] = false
-    }
-    if (tuesday   in allDays) {
-      console.log('accounting for', tuesday, 'within week break', weekBreak)
-      allDays[tuesday] = false
-    }
-    if (wednesday in allDays) {
-      console.log('accounting for', wednesday, 'within week break', weekBreak)
-      allDays[wednesday] = false
-    }
-    if (thursday  in allDays) {
-      console.log('accounting for', thursday, 'within week break', weekBreak)
-      allDays[thursday] = false
-    }
-    if (friday    in allDays) {
-      console.log('accounting for', friday, 'within week break', weekBreak)
-      allDays[friday] = false
-    }
+    if (monday    in allDays) allDays[monday]    = false
+    if (tuesday   in allDays) allDays[tuesday]   = false
+    if (wednesday in allDays) allDays[wednesday] = false
+    if (thursday  in allDays) allDays[thursday]  = false
+    if (friday    in allDays) allDays[friday]    = false
   })
 
   holidays.forEach(function(holiday){
@@ -131,20 +110,13 @@ const calculateNumberOfDaysInSession = function(from, to){
     if (date in allDays) allDays[date] = false
   })
 
-  console.log('allDays', allDays)
   return Object.keys(allDays).filter(day => allDays[day]).length
 }
 
 const getHolidayObservationDate = function(holiday){
   const day = moment(holiday).format('dddd')
-  if (day === 'Sunday') {
-    console.log('holiday observed 1 day after', holiday, moment(holiday).add(1, 'day'))
-    return moment(holiday).add(1, 'day')
-  }
-    if (day === 'Saturday') {
-    console.log('holiday observed 1 day before', holiday, moment(holiday).subtract(1, 'day'))
-    return moment(holiday).subtract(1, 'day')
-  }
+  if (day === 'Sunday') return moment(holiday).add(1, 'day')
+  if (day === 'Saturday') return moment(holiday).subtract(1, 'day')
   return moment(holiday)
 }
 
