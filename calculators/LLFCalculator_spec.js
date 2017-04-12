cf7346f081b07ad1a500492527ee618ccb91a568
Enrique Perez <enriquexp19@gmail.com>
2017-04-12 15:33:28 -0700

    removed unnecessary outputs

--- calculators/LLFCalculator_spec.js
+++ calculators/LLFCalculator_spec.js
@@ -1,27 +1,23 @@
 const expect = require('chai').expect
-const stipendCalculator = require('./LLFCalculator')
+const LLFCalculator = require('./LLFCalculator')
 
-describe('LFFCalculator', function(){
+describe.only('LFFCalculator', function(){
   it('calculates the right amount of money owed for a full successful term at Learner\'s guild for the LLF', function(){
     expect(LLFCalculator({
       learnerLivingFundStipendAmount: 1384600,
       takingTheLaptopStipend: true,
     })).to.eql({
       // 15692 / (15692 * 8.5%)
-      'livingFundMonthlyPercentage': 8.5,
-      'laptopStipened':              true,
-      'livingFundPaymentTerm':       36,
+      'livingFundMonthlyPercentage': 8.5
     })
   })
   it('calculates the right amount of money owed for three sessions at Learner\'s guild before exiting for the LLF', function(){
     expect(LLFCalculator({
       learnerLivingFundStipendAmount: 946130,
-      takingTheLaptopStipend: true,
+      takingTheLaptopStipend: false,
     })).to.eql({
       // 15692 / (15692 * 8.5%)
-      'livingFundMonthlyPercentage': 5.12,
-      'laptopStipened':              true,
-      'livingFundPaymentTerm':       36,
+      'livingFundMonthlyPercentage': 5.12
     })
   })
 })
