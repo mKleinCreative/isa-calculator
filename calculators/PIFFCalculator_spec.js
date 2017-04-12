ec8e474049b64499f76d65f5cfff77b9bd6a3aee
Enrique Perez <enriquexp19@gmail.com>
2017-04-12 15:04:32 -0700

    Fixed input arguments

--- calculators/PIFFCalculator_spec.js
+++ calculators/PIFFCalculator_spec.js
@@ -3,7 +3,7 @@ const stipendCalculator = require('./PIFFCalculator')
 
 describe('PIFFCalculator', function(){
   it('calculates the right amount of money owed for a full successful term at Learner\'s guild for the PIFF', function(){
-    expect(PIFFCalculator({ '2017-06-19', '2018-03-30' })).to.eql({
+    expect(PIFFCalculator( '2017-06-19', '2018-03-30' )).to.eql({
       'programFeeMonthlyPercentage':        12.5, // STATIC
       'laptopStipened':                     true,
       'session1FundingAmount':              1700000,
@@ -16,7 +16,7 @@ describe('PIFFCalculator', function(){
     })
   })
   it('calculates the right amount of money owed for three sessions at Learner\'s guild before exiting for the PIFF', function(){
-    expect(PIFFCalculator({ '2017-06-19', '2017-12-21' })).to.eql({
+    expect(PIFFCalculator( '2017-06-19', '2017-12-21' )).to.eql({
       'programFeeMonthlyPercentage':        12.5,
       'session1FundingAmount':              1700000,
       'session2FundingAmount':              850000,
