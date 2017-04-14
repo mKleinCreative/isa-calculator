07e3436754c52e8222007b9fed369e15bd4969f2
Michael Klein <miketheklein@gmail.com>
2017-04-13 17:48:28 -0700

    WIP

--- calculators/PIFFCalculator.js
+++ calculators/PIFFCalculator.js
@@ -37,17 +37,7 @@ module.exports = function(spec){
     numberOfDaysInSession4,
   } = spec
 
-  let
-    currentSession,
-    session1FundingAmount,
-    session2FundingAmount,
-    session3FundingAmount,
-    session4FundingAmount,
-    payItForwardFundPaymentTerm,
-    payItForwardFundTotalFundingAmount,
-    rebate
-
-  if ( earlyExitDate === session4EndDate) return {
+  if (!earlyExitDate) return {
     session1FundingAmount:          session1MaxFundingAmount,
     session2FundingAmount:          session2MaxFundingAmount,
     session3FundingAmount:          session3MaxFundingAmount,
@@ -56,139 +46,299 @@ module.exports = function(spec){
     payItForwardFundPaymentTerm:    36,
     payItForwardFundTotalFundingAmount: (
       session1MaxFundingAmount +
-      session2MaxFundingAmount
+      session2MaxFundingAmount +
+      session3MaxFundingAmount +
+      session4MaxFundingAmount +
+      completeProgramFundingAmountRebate
     ),
   }
 
-  const firstSession = daysAttended => {
-    const percentageComplete = numberOfDaysInSession1 / daysAttended
-    let percentageOwed = 0
 
-    if( percentageComplete <= 0.5 ) {
-      percentageOwed = 0
-    } else if( percentageComplete < 0.6 ) {
-      percentageOwed = session1MaxFundingAmount / percentageComplete
-    } else if( percentageComplete >= 0.6 ){
-      percentageOwed = session1MaxFundingAmount
-    } return percentageOwed
-  }
 
-  const secondSession = daysAttended => {
-    const percentageComplete = numberOfDaysInSession2 / daysAttended
-    let percentageOwed = 0
+  const sessions = [
+    {
+      startDate: session1StartDate,
+      endDate: session1EndDate,
+      numberOfSchoolDays: numberOfDaysInSession1,
+      maxFundingAmount: session1MaxFundingAmount,
+      maxPaymentTerm: session1PaymentTermLength,
+    },
+    {
+      startDate: session2StartDate,
+      endDate: session2EndDate,
+      numberOfSchoolDays: numberOfDaysInSession2,
+      maxFundingAmount: session2MaxFundingAmount,
+      maxPaymentTerm: session2PaymentTermLength,
+    },
+    {
+      startDate: session3StartDate,
+      endDate: session3EndDate,
+      numberOfSchoolDays: numberOfDaysInSession3,
+      maxFundingAmount: session3MaxFundingAmount,
+      maxPaymentTerm: session3PaymentTermLength,
+    },
+    {
+      startDate: session4StartDate,
+      endDate: session4EndDate,
+      numberOfSchoolDays: numberOfDaysInSession4,
+      maxFundingAmount: session4MaxFundingAmount,
+      maxPaymentTerm: session4PaymentTermLength,
+    },
+  ]
 
-    if( percentageComplete < 0.6 ) {
-      percentageOwed = session2MaxFundingAmount / percentageComplete
-    } else if( percentageComplete >= 0.6 ){
-      percentageOwed = session2MaxFundingAmount
-    } return percentageOwed
-  }
+  const exitSession = (
+    moment(earlyExitDate).isBetween(session1StartDate, session1EndDate) ? 1 :
+    moment(earlyExitDate).isBetween(session2StartDate, session2EndDate) ? 2 :
+    moment(earlyExitDate).isBetween(session3StartDate, session3EndDate) ? 3 :
+    moment(earlyExitDate).isBetween(session4StartDate, session4EndDate) ? 4 :
+    false
+  )
+  if (!exitSession) throw new Error('invalid earlyExitDate')
 
-  const thirdSession = daysAttended => {
-    // const percentageComplete = Math.round((daysAttended / numberOfDaysInSession3) * 100) / 100
-    // const percentageComplete = Math.round((daysAttended / numberOfDaysInSession3) * 1000) / 1000
-    const percentageComplete = daysAttended / numberOfDaysInSession3
-    console.log('percentageComplete =', daysAttended,' / ',numberOfDaysInSession3, ' === ', percentageComplete)
-    console.log('session3MaxFundingAmount * percentageComplete', session3MaxFundingAmount, ' * ', percentageComplete, ' === ', (session3MaxFundingAmount * percentageComplete))
-    return percentageComplete <= 0.6
-      ? session3MaxFundingAmount * percentageComplete
-      : session3MaxFundingAmount
-  }
+  sessions.forEach(function(session, index){
+    const sessionNumber = index + 1;
+    session.numberOfSchoolDaysAttended =
+      sessionNumber < exitSession   ? session.numberOfSchoolDays :
+      sessionNumber === exitSession ? schoolDayCalculator(session.startDate, earlyExitDate) :
+      0
+    session.percentageComplete = session.numberOfSchoolDaysAttended / session.numberOfSchoolDays
+    session.fundingAmount = session.maxFundingAmount * session.percentageComplete
+    session.paymentTerm = session.maxPaymentTerm * session.percentageComplete
+  })
 
-  const fourthSession = daysAttended => {
-    const percentageComplete = daysAttended / numberOfDaysInSession4
-    let percentageOwed = 0
-    if( percentageComplete < 0.6 ) {
-      percentageOwed = session4MaxFundingAmount / percentageComplete
-    } else if( percentageComplete >= 0.6 ){
-      percentageOwed = session4MaxFundingAmount
-    } return percentageOwed
-  }
 
-  // const calculatePaymentTerm = (currentSession, currentSessionStart, earlyExitDate) => {
-  //   const
-  // }
+  console.log('sessions', sessions)
 
-  const calculateFundingAmountOwed = (currentSession, currentSessionStart, earlyExitDate) => {
-    console.log('calculateFundingAmountOwed', {currentSession, currentSessionStart, earlyExitDate})
-    let daysAttended = schoolDayCalculator(currentSessionStart, earlyExitDate)
-    let percentageOwed = undefined
+  const session1FundingAmount = sessions[0].fundingAmount
+  const session2FundingAmount = sessions[1].fundingAmount
+  const session3FundingAmount = sessions[2].fundingAmount
+  const session4FundingAmount = sessions[3].fundingAmount
+  const payItForwardFundTotalFundingAmount = (
+    session1FundingAmount +
+    session2FundingAmount +
+    session3FundingAmount +
+    session4FundingAmount
+  )
 
-    return {
-      '1': firstSession,
-      '2': secondSession,
-      '3': thirdSession,
-      '4': fourthSession
-    }[ currentSession ]( daysAttended )
-  }
-
-  if (moment(earlyExitDate).isBetween(session1StartDate, session1EndDate)){
-    currentSession = 1
-    session1FundingAmount = calculateFundingAmountOwed(currentSession, session1StartDate, earlyExitDate)
-    session2FundingAmount = 0
-    session3FundingAmount = 0
-    session4FundingAmount = session4MaxFundingAmount
-    recievedPayItForwardFundRebate = false
-    // payItForwardFundPaymentTerm = calculatePaymentTerm(currentSession, session1StartDate, earlyExitDate)
-    payItForwardFundTotalFundingAmount = session1FundingAmount
-  }
-
-  else if (moment(earlyExitDate).isBetween(session2StartDate, session2EndDate)) {
-    currentSession = 2
-    session1FundingAmount = session1MaxFundingAmount
-    session2FundingAmount = calculateFundingAmountOwed(currentSession, session2StartDate, earlyExitDate)
-    session3FundingAmount = 0
-    session4FundingAmount = session4MaxFundingAmount
-    recievedPayItForwardFundRebate = false
-    // payItForwardFundPaymentTerm = calculatePaymentTerm(currentSession, session1StartDate, earlyExitDate)
-    payItForwardFundTotalFundingAmount = session1MaxFundingAmount + session2FundingAmount
-  }
-
-  else if (moment(earlyExitDate).isBetween(session3StartDate, session3EndDate)) {
-    currentSession = 3
-    session1FundingAmount = session1MaxFundingAmount
-    session2FundingAmount = session2MaxFundingAmount
-    session3FundingAmount = calculateFundingAmountOwed(
-      currentSession,
-      session3StartDate,
-      earlyExitDate
-    )
-    session4FundingAmount = session4MaxFundingAmount
-    recievedPayItForwardFundRebate = false
-    // payItForwardFundPaymentTerm = calculatePaymentTerm(currentSession, session1StartDate, earlyExitDate)
-    payItForwardFundTotalFundingAmount = (
-      session1MaxFundingAmount +
-      session2MaxFundingAmount +
-      session3FundingAmount
-    )
-    console.log('payItForwardFundTotalFundingAmount = ',
-      session1MaxFundingAmount, ' + ',
-      session2MaxFundingAmount, ' + ',
-      session3FundingAmount, ' === ',
-      payItForwardFundTotalFundingAmount
-    )
-    console.log('session3FundingAmount:', session3FundingAmount)
-  }
-
-  else if (moment(earlyExitDate).isBetween(session4StartDate, session4EndDate)){
-    currentSession = 4
-    session1FundingAmount = session1MaxFundingAmount
-    session2FundingAmount = session2MaxFundingAmount
-    session3FundingAmount = session3MaxFundingAmount
-    session4FundingAmount = session4MaxFundingAmount
-    recievedPayItForwardFundRebate = false
-    payItForwardFundPaymentTerm = 42
-    payItForwardFundTotalFundingAmount = (session1MaxFundingAmount + session2MaxFundingAmount + session3MaxFundingAmount + session4MaxFundingAmount)
-
-  }
+  const payItForwardFundPaymentTerm = (
+    sessions[0].paymentTerm +
+    sessions[1].paymentTerm +
+    sessions[2].paymentTerm +
+    sessions[3].paymentTerm
+  )
 
   return {
     session1FundingAmount,
     session2FundingAmount,
     session3FundingAmount,
     session4FundingAmount,
-    recievedPayItForwardFundRebate,
-    payItForwardFundPaymentTerm,
+    recievedPayItForwardFundRebate: false,
     payItForwardFundTotalFundingAmount,
+    payItForwardFundPaymentTerm,
   }
+
+
+  // ---- old 1
+
+  //
+  //
+  //
+  //
+  // const exitSession = (
+  //   moment(earlyExitDate).isBetween(session1StartDate, session1EndDate) ? 1 :
+  //   moment(earlyExitDate).isBetween(session2StartDate, session2EndDate) ? 2 :
+  //   moment(earlyExitDate).isBetween(session3StartDate, session3EndDate) ? 3 :
+  //   moment(earlyExitDate).isBetween(session4StartDate, session4EndDate) ? 4 :
+  //   false
+  // )
+  // if (![1,2,3,4].includes(exitSession)) throw new Error('invalid earlyExitDate')
+  //
+  // const exitSessionStartDate = {
+  //   1: session1StartDate,
+  //   2: session2StartDate,
+  //   3: session3StartDate,
+  //   4: session4StartDate,
+  // }[exitSession]
+  //
+  // const numberOfDaysInExitSession = {
+  //   1: numberOfDaysInSession1,
+  //   2: numberOfDaysInSession2,
+  //   3: numberOfDaysInSession3,
+  //   4: numberOfDaysInSession4,
+  // }[exitSession]
+  //
+  // const exitSessionDaysAttended = schoolDayCalculator(exitSessionStartDate, earlyExitDate)
+  //
+  // const exitSessionPercentage = exitSessionDaysAttended / numberOfDaysInExitSession
+  //
+  //
+  // const session1FundingAmount = exitSession === 1
+  //   ?
+  //   : session1MaxFundingAmount
+  // const session2FundingAmount = exitSession === 2
+  //   ?
+  //   : session2MaxFundingAmount
+  // const session3FundingAmount = exitSession === 3
+  //   ?
+  //   : session3MaxFundingAmount
+  // const session4FundingAmount = exitSession === 4
+  //   ?
+  //   : session4MaxFundingAmount
+  //
+  // return {
+  //   exitSession,
+  //   exitSessionDaysAttended,
+  //   exitSessionPercentage,
+  //   session1FundingAmount,
+  //   session2FundingAmount,
+  //   session3FundingAmount,
+  //   session4FundingAmount,
+  // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // ---- old 2
+
+  //
+  // const firstSession = daysAttended => {
+  //   const percentageComplete = numberOfDaysInSession1 / daysAttended
+  //   let percentageOwed = 0
+  //
+  //   if( percentageComplete <= 0.5 ) {
+  //     percentageOwed = 0
+  //   } else if( percentageComplete < 0.6 ) {
+  //     percentageOwed = session1MaxFundingAmount / percentageComplete
+  //   } else if( percentageComplete >= 0.6 ){
+  //     percentageOwed = session1MaxFundingAmount
+  //   } return percentageOwed
+  // }
+  //
+  // const secondSession = daysAttended => {
+  //   const percentageComplete = numberOfDaysInSession2 / daysAttended
+  //   let percentageOwed = 0
+  //
+  //   if( percentageComplete < 0.6 ) {
+  //     percentageOwed = session2MaxFundingAmount / percentageComplete
+  //   } else if( percentageComplete >= 0.6 ){
+  //     percentageOwed = session2MaxFundingAmount
+  //   } return percentageOwed
+  // }
+  //
+  // const thirdSession = daysAttended => {
+  //   // const percentageComplete = Math.round((daysAttended / numberOfDaysInSession3) * 100) / 100
+  //   // const percentageComplete = Math.round((daysAttended / numberOfDaysInSession3) * 1000) / 1000
+  //   const percentageComplete = daysAttended / numberOfDaysInSession3
+  //   console.log('percentageComplete =', daysAttended,' / ',numberOfDaysInSession3, ' === ', percentageComplete)
+  //   console.log('session3MaxFundingAmount * percentageComplete', session3MaxFundingAmount, ' * ', percentageComplete, ' === ', (session3MaxFundingAmount * percentageComplete))
+  //   return percentageComplete <= 0.6
+  //     ? session3MaxFundingAmount * percentageComplete
+  //     : session3MaxFundingAmount
+  // }
+  //
+  // const fourthSession = daysAttended => {
+  //   const percentageComplete = daysAttended / numberOfDaysInSession4
+  //   let percentageOwed = 0
+  //   if( percentageComplete < 0.6 ) {
+  //     percentageOwed = session4MaxFundingAmount / percentageComplete
+  //   } else if( percentageComplete >= 0.6 ){
+  //     percentageOwed = session4MaxFundingAmount
+  //   } return percentageOwed
+  // }
+  //
+  // // const calculatePaymentTerm = (currentSession, currentSessionStart, earlyExitDate) => {
+  // //   const
+  // // }
+  //
+  // const calculateFundingAmountOwed = (currentSession, currentSessionStart, earlyExitDate) => {
+  //   console.log('calculateFundingAmountOwed', {currentSession, currentSessionStart, earlyExitDate})
+  //   let daysAttended = schoolDayCalculator(currentSessionStart, earlyExitDate)
+  //   let percentageOwed = undefined
+  //
+  //   return {
+  //     '1': firstSession,
+  //     '2': secondSession,
+  //     '3': thirdSession,
+  //     '4': fourthSession
+  //   }[ currentSession ]( daysAttended )
+  // }
+  //
+  // if (moment(earlyExitDate).isBetween(session1StartDate, session1EndDate)){
+  //   currentSession = 1
+  //   session1FundingAmount = calculateFundingAmountOwed(currentSession, session1StartDate, earlyExitDate)
+  //   session2FundingAmount = 0
+  //   session3FundingAmount = 0
+  //   session4FundingAmount = session4MaxFundingAmount
+  //   recievedPayItForwardFundRebate = false
+  //   // payItForwardFundPaymentTerm = calculatePaymentTerm(currentSession, session1StartDate, earlyExitDate)
+  //   payItForwardFundTotalFundingAmount = session1FundingAmount
+  // }
+  //
+  // else if (moment(earlyExitDate).isBetween(session2StartDate, session2EndDate)) {
+  //   currentSession = 2
+  //   session1FundingAmount = session1MaxFundingAmount
+  //   session2FundingAmount = calculateFundingAmountOwed(currentSession, session2StartDate, earlyExitDate)
+  //   session3FundingAmount = 0
+  //   session4FundingAmount = session4MaxFundingAmount
+  //   recievedPayItForwardFundRebate = false
+  //   // payItForwardFundPaymentTerm = calculatePaymentTerm(currentSession, session1StartDate, earlyExitDate)
+  //   payItForwardFundTotalFundingAmount = session1MaxFundingAmount + session2FundingAmount
+  // }
+  //
+  // else if (moment(earlyExitDate).isBetween(session3StartDate, session3EndDate)) {
+  //   currentSession = 3
+  //   session1FundingAmount = session1MaxFundingAmount
+  //   session2FundingAmount = session2MaxFundingAmount
+  //   session3FundingAmount = calculateFundingAmountOwed(
+  //     currentSession,
+  //     session3StartDate,
+  //     earlyExitDate
+  //   )
+  //   session4FundingAmount = session4MaxFundingAmount
+  //   recievedPayItForwardFundRebate = false
+  //   // payItForwardFundPaymentTerm = calculatePaymentTerm(currentSession, session1StartDate, earlyExitDate)
+  //   payItForwardFundTotalFundingAmount = (
+  //     session1MaxFundingAmount +
+  //     session2MaxFundingAmount +
+  //     session3FundingAmount
+  //   )
+  //   console.log('payItForwardFundTotalFundingAmount = ',
+  //     session1MaxFundingAmount, ' + ',
+  //     session2MaxFundingAmount, ' + ',
+  //     session3FundingAmount, ' === ',
+  //     payItForwardFundTotalFundingAmount
+  //   )
+  //   console.log('session3FundingAmount:', session3FundingAmount)
+  // }
+  //
+  // else if (moment(earlyExitDate).isBetween(session4StartDate, session4EndDate)){
+  //   currentSession = 4
+  //   session1FundingAmount = session1MaxFundingAmount
+  //   session2FundingAmount = session2MaxFundingAmount
+  //   session3FundingAmount = session3MaxFundingAmount
+  //   session4FundingAmount = session4MaxFundingAmount
+  //   recievedPayItForwardFundRebate = false
+  //   payItForwardFundPaymentTerm = 42
+  //   payItForwardFundTotalFundingAmount = (session1MaxFundingAmount + session2MaxFundingAmount + session3MaxFundingAmount + session4MaxFundingAmount)
+  //
+  // }
+  //
+  // return {
+  //   session1FundingAmount,
+  //   session2FundingAmount,
+  //   session3FundingAmount,
+  //   session4FundingAmount,
+  //   recievedPayItForwardFundRebate,
+  //   payItForwardFundPaymentTerm,
+  //   payItForwardFundTotalFundingAmount,
+  // }
 }
--- calculators/PIFFCalculator_spec.js
+++ calculators/PIFFCalculator_spec.js
@@ -6,7 +6,6 @@ describe.only('PIFFCalculator', function(){
   it('calculates the right amount of money owed for a full successful term at Learner\'s guild for the PIFF', function(){
     expect(PIFFCalculator({
       programStartDate:                   '2017-06-19',
-      earlyExitDate:                      '2018-03-30',
       cancellationDate:                   '2017-07-24',
       session1StartDate:                  '2017-06-19',
       session1EndDate:                    '2017-08-25',
@@ -28,6 +27,30 @@ describe.only('PIFFCalculator', function(){
       recievedPayItForwardFundRebate:     true,
       payItForwardFundPaymentTerm:        36,
       payItForwardFundTotalFundingAmount: 2550000,
+    })
+  })
+
+  context('when given an invalid early exit date', function(){
+    it('should throw an error', function(){
+      expect(function(){
+        PIFFCalculator({
+          programStartDate:       '2017-06-19',
+          earlyExitDate:          '1999-01-01', // BAD DATE
+          cancellationDate:       '2017-07-24',
+          session1StartDate:      '2017-06-19',
+          session1EndDate:        '2017-08-25',
+          session2StartDate:      '2017-08-28',
+          session2EndDate:        '2017-11-03',
+          session3StartDate:      '2017-11-06',
+          session3EndDate:        '2018-01-19',
+          session4StartDate:      '2018-01-22',
+          session4EndDate:        '2018-03-30',
+          numberOfDaysInSession1: 49,
+          numberOfDaysInSession2: 48,
+          numberOfDaysInSession3: 45,
+          numberOfDaysInSession4: 48,
+        })
+      }).to.throw('invalid earlyExitDate')
     })
   })
 
@@ -59,7 +82,7 @@ describe.only('PIFFCalculator', function(){
     })
   })
 
-  it.only('calculates the right amount of money owed for three sessions at Learner\'s guild before exiting for the PIFF', function(){
+  it('calculates the right amount of money owed for three sessions at Learner\'s guild before exiting for the PIFF', function(){
     expect(PIFFCalculator({
       programStartDate:                   '2017-06-19',
       earlyExitDate:                      '2017-12-07',
